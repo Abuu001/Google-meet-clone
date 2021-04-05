6997e2e9604569cf411ce81c5c0d6aec2af35117
Abuu001 <abulugz@gmail.com>
2021-04-05 21:37:18 +0300

    redis config

---
+++ config/redis.js
@@ -0,0 +1,15 @@
+require('dotenv').config({path : "../.env"})
+let redis = require('redis');
+const REDIS_PORT = process.env.REDIS_PORT || 6379;
+
+let client = redis.createClient(REDIS_PORT);
+
+// client.on('error',(error)=>{
+//     console.log(error)
+// })
+
+client.on("connect", () => {
+    console.log("Connected to Redis.... ");
+});
+  
+module.exports= client;
