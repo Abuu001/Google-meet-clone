63e1dccd974d6a55cf0bbcc393ba9ebcd1f2f6b0
Abuu001 <abulugz@gmail.com>
2021-04-06 22:44:41 +0300

    ready for heroku

--- client/src/components/utils/apiEndPoints.js
+++ client/src/components/utils/apiEndPoints.js
@@ -1,6 +1,6 @@
 // export const BASE_URL="http://localhost:3008"
 export const SAVE_CALL_ID="/api/save-call-id" 
-export const GET_CALL_ID="/api/get-call-id";
+export const GET_CALL_ID="/api/get-call-id"; 
 
 
 
