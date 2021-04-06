7277f64f73fb17fa890f9ced152d26d6f9c41381
Abuu001 <abulugz@gmail.com>
2021-04-06 23:10:37 +0300

    ready for heroku

--- config/redis.js
+++ config/redis.js
@@ -1,15 +1,37 @@
 require('dotenv').config({path : "../.env"})
-let redis = require('redis');
+// let redis = require("redis").createClient();
+let client;
 const REDIS_PORT = process.env.REDIS_PORT || 6379;
 
-let client = redis.createClient(REDIS_PORT);
+if (process.env.REDISTOGO_URL) {
+    
+    let rtg   = JSON.parse(process.env.REDISTOGO_URL);
+    client = require("redis").createClient(rtg.port, rtg.hostname);
 
-// client.on('error',(error)=>{
-//     console.log(error)
-// })
+    redis.auth(rtg.auth.split(":")[1]);
+  } else {
+    client = require("redis").createClient(REDIS_PORT);
+  }
 
-client.on("connect", () => {
-    console.log("Connected to Redis.... ");
-});
-  
+// client.on("connect", () => {
+//     console.log("Connected to Redis.... ");
+// });
+   
 module.exports= client;
+
+
+// require('dotenv').config({path : "../.env"})
+// let redis = require('redis');
+// const REDIS_PORT = process.env.REDIS_PORT || 6379;
+
+// let client = redis.createClient(REDIS_PORT);
+
+// // client.on('error',(error)=>{
+// //     console.log(error)
+// // })
+
+// client.on("connect", () => {
+//     console.log("Connected to Redis.... ");
+// });
+  
+// module.exports= client;
