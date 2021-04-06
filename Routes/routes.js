56a07c143094908bb0ace395acad795edf896a79
Abuu001 <abulugz@gmail.com>
2021-04-06 21:06:49 +0300

    add a get route call

--- Routes/routes.js
+++ Routes/routes.js
@@ -2,6 +2,6 @@ const router = require('express').Router()
 const controller = require('../controllers/controllers')
 
 router.post('/api/save-call-id',controller.saveCallId);
-router.post('/api/get-call-id/:id',controller.getCallId);
+router.get('/api/get-call-id/:id',controller.getCallId);
 
 module.exports=router
