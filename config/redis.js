f0472fe6f1a399ef1bb0d5688923b0ad4654f410
Abuu001 <abulugz@gmail.com>
2021-04-07 14:23:56 +0300

    ready for heroku

--- config/redis.js
+++ config/redis.js
@@ -15,7 +15,7 @@ let redis = require('redis');
 const REDIS_PORT1 = process.env.REDIS_PORT;
 const REDIS_URL=process.env.REDIS_URL; 
 
-let client = redis.createClient(REDIS_PORT1 || REDIS_URL);
+let client = redis.createClient( REDIS_URL || process.env.REDISTOGO_URL || REDIS_PORT1);
 
 // client.on('error',(error)=>{
 //     console.log(error)
